2b6ffef67da809fd9bd30701e7503d8b8f948922
Marvin <mvt5393@yahoo.com>
2019-05-26 12:48:37 -0400

    Trial Commit

---
+++ File.swift
@@ -0,0 +1,9 @@
+// This is a trial file
+//  File.swift
+//  GitTrial
+//
+//  Created by Marvin Tailor on 5/26/19.
+//  Copyright © 2019 Marvin Tailor. All rights reserved.
+//
+
+import Foundation
